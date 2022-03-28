d9670713df0c6f1ec87f47c4dd8cf7e1aa06c14a
hikari.ftg <me@hikariatama.ruftg>
2022-03-28 15:50:22 +0000

    Add GeekTG backward compatibility layer

--- hikka/compat/geek.py
+++ hikka/compat/geek.py
@@ -7,10 +7,35 @@
 #
 # 🔒 Licensed under the GNU GPLv3
 # 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+import re
 
 
 def compat(code: str) -> str:
     """Reformats modules, built for GeekTG to work with Hikka"""
-    code = code.replace("GeekInlineQuery", "InlineQuery")
-
+    code = "\n".join(
+        [
+            re.sub(
+                r"^( *)from \.\.inline import (.+)$",
+                r"\1from ..inline.types import \2",
+                re.sub(
+                    r"^( *)from \.\.inline import rand, ?(.+)$",
+                    r"\1from ..inline.types import \2\n\1from ..utils import rand",
+                    re.sub(
+                        r"^( *)from \.\.inline import (.+), ?rand[^,]+$",
+                        r"\1from ..inline.types import \2\n\1from ..utils import rand",
+                        re.sub(
+                            r"^( *)from \.\.inline import (.+), ?rand, ?(.+)$",
+                            r"\1from ..inline.types import \2, \3\n\1from ..utils import rand",
+                            line.replace("GeekInlineQuery", "InlineQuery"),
+                            flags=re.M,
+                        ),
+                        flags=re.M,
+                    ),
+                    flags=re.M,
+                ),
+                flags=re.M,
+            )
+            for line in code.splitlines()
+        ]
+    )
     return code
