7c03a91e2e1b2ddfb57de491cea9d0f839f7dab6
AmoreForever <103936585+AmoreForever@users.noreply.github.com>
2022-05-16 23:16:28 +0500

    Update hikka_security.py

--- hikka/modules/hikka_security.py
+++ hikka/modules/hikka_security.py
@@ -44,7 +44,7 @@ class HikkaSecurityMod(loader.Module):
     """Control security settings"""
 
     strings = {
-        "name": "AmorwSecurity",
+        "name": "AmoreSecurity",
         "no_command": "🚫 <b>Command </b><code>{}</code><b> not found!</b>",
         "permissions": "🔐 <b>Here you can configure permissions for </b><code>{}{}</code>",
         "close_menu": "🙈 Close this menu",
