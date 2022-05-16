664411f42f15ff77923e0a647e32edaf0e8bcbf8
AmoreForever <103936585+AmoreForever@users.noreply.github.com>
2022-05-17 01:16:11 +0500

    Update query_gallery.py

--- hikka/inline/query_gallery.py
+++ hikka/inline/query_gallery.py
@@ -121,7 +121,7 @@ class QueryGallery(InlineUnit):
                     title=i["title"],
                     description=i["description"],
                     input_message_content=InputTextMessageContent(
-                        f"🌘 <b>Loading Hikka gallery...</b>\n<i>#id: {id_}</i>",
+                        f"🌌 <b>Loading Amore gallery...</b>\n<i>#id: {id_}</i>",
                         "HTML",
                         disable_web_page_preview=True,
                     ),
