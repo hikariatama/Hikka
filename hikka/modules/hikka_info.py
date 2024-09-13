781c0f8b96c6165dd29ea4cd41aa6f27b6062261
Who? <155328415+coddrago@users.noreply.github.com>
2024-09-13 12:23:47 +0700

    Update hikka_info.py

--- hikka/modules/hikka_info.py
+++ hikka/modules/hikka_info.py
@@ -26,7 +26,7 @@ class HikkaInfoMod(loader.Module):
             ),
             loader.ConfigValue(
                 "custom_button",
-                ["🌘 Support chat", "https://t.me/hikka_talks"],
+                ["🌘 Support chat", "https://t.me/WhoBlogs_chat"],
                 lambda: self.strings("_cfg_cst_btn"),
                 validator=loader.validators.Union(
                     loader.validators.Series(fixed_len=2),
