39f8d58130d7e2720ac46df8703278f1d891d056
Who? <155328415+coddrago@users.noreply.github.com>
2024-09-19 15:22:10 +0700

    Update updater.py

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -39,7 +39,7 @@ class UpdaterMod(loader.Module):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
                 "GIT_ORIGIN_URL",
-                "https://github.com/hikariatama/Hikka",
+                "https://github.com/coddrago/Hikka",
                 lambda: self.strings("origin_cfg_doc"),
                 validator=loader.validators.Link(),
             )
