40c0cbe5771d9f022d8193bebe4302ba828d5fd3
Who? <155328415+coddrago@users.noreply.github.com>
2024-10-05 22:10:02 +0700

    Update settings.py

--- hikka/modules/settings.py
+++ hikka/modules/settings.py
@@ -61,7 +61,7 @@ class CoreMod(loader.Module):
             )
             + (
 
-                  ("\n\n<emoji document_id=5287454910059654880>❤️</emoji> <b>Desinger: t.me/tr4mq</b>")
+                  ("\n\n<emoji document_id=5287454910059654880>❤️</emoji> <b>Designer: t.me/tr4mq</b>")
                   if random.choice([0, 0, 0, 1]) == 1
                   else ""
               ),
