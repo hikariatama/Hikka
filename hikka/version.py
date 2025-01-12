51097e1aa00829c806eedf2636b440abf78afffc
arion14888 <rertrert228890@gmail.com>
2025-01-12 22:43:39 +0300

    Update version.py

--- hikka/version.py
+++ hikka/version.py
@@ -5,7 +5,7 @@
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
-__version__ = (1, 6, 3)
+__version__ = (1, 6, 5)
 
 import os
 
